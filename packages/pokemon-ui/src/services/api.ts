107f6a8f1d7bfe0606352a2a99a8e454caca4535
Elec0 <elec08215@gmail.com>
2024-10-11 16:52:07 -0700

    Refactor api.ts to include functions for fetching and assigning Pokémon to a profile

--- packages/pokemon-ui/src/services/api.ts
+++ packages/pokemon-ui/src/services/api.ts
@@ -1,4 +1,4 @@
-import { Profile } from "./types";
+import { Pokemon, Profile } from "./types";
 
 export const fetchPokemon = async () => {
   const response = await fetch(`${import.meta.env.VITE_API_URL}/api/pokemon`);
@@ -36,5 +36,38 @@ export const createProfile = async (name: string): Promise<Profile> => {
 export const deleteProfile = async (id: number): Promise<void> => {
   await fetch(`${import.meta.env.VITE_API_URL}/api/profiles/${id}`, {
     method: 'DELETE',
+  });
+};
+
+/**
+ * Fetches the Pokémon associated with a profile by ID.
+ * 
+ * Frontend expects a full pokemon object
+ */
+export const fetchProfilePokemon = async (id: number): Promise<Pokemon[]> => {
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/api/profiles/${id}/pokemon`);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  const data = await response.json();
+  return data.map((pokemon: any) => ({
+    id: pokemon.id,
+    name: pokemon.name,
+    types: pokemon.types,
+    image: pokemon?.imageUrl, // Assuming the backend returns an imageUrl field
+  }));
+
+};
+
+/**
+ * Assigns a list of Pokémon to a profile by ID.
+ */
+export const assignPokemonToProfile = async (profileId: number, pokemonIds: number[]): Promise<void> => {
+  await fetch(`${import.meta.env.VITE_API_URL}/api/profiles/${profileId}/pokemon`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(pokemonIds),
   });
 };
